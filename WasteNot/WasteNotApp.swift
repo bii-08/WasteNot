113f7572bad1c22ae56cfd264662a068ec762beb
bii-08 <luuthithanhtam08@gmail.com>
2024-07-15 13:40:37 +1000

    initial commit

---
+++ WasteNot/WasteNotApp.swift
@@ -0,0 +1,17 @@
+//
+//  WasteNotApp.swift
+//  WasteNot
+//
+//  Created by LUU THANH TAM on 2024/07/15.
+//
+
+import SwiftUI
+
+@main
+struct WasteNotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
